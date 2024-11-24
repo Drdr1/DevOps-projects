9a85ebf9564ff5f198bd00d77148a1d34886838f
ahmed darder <91543865+Drdr1@users.noreply.github.com>
2024-11-24 20:46:01 +0200

    Add files via upload

---
+++ aws-three-tier-web-architecture-workshop/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.c34yqiksmsk3.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Darder00#',
+    DB_DATABASE : 'database-1-instance-1.c34yqiksmsk3.us-east-1.rds.amazonaws.com'
+});
